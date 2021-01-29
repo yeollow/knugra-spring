d957186f461020290ae93eca21863abdc49f0f99
yeollow <ljy2784437@gmail.com>
2021-01-29 13:44:36 +0900

    build: springBootProject

---
+++ src/main/java/com/springapp/knugra/KnugraApplication.java
@@ -0,0 +1,13 @@
+package com.springapp.knugra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnugraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnugraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springapp/knugra/KnugraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapp.knugra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnugraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
